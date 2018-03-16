dc4ac52a77caf1e62b00ed329cb7640584de723e
ACJTeam <magicobuendia@hotmail.com>
2018-03-16 21:46:25 +0100

    Update LanguageSelection.py

--- lib/python/Screens/LanguageSelection.py
+++ lib/python/Screens/LanguageSelection.py
@@ -83,7 +83,7 @@ class LanguageSelection(Screen):
 	def updateList(self):
 		languageList = language.getLanguageList()
 		if not languageList: # no language available => display only english
-			list = [ LanguageEntryComponent("en", "English", "en_EN") ]
+			list = [ LanguageEntryComponent("es", "Spanish", "es_ES") ]
 		else:
 			list = [ LanguageEntryComponent(file = x[1][2].lower(), name = x[1][0], index = x[0]) for x in languageList]
 		self.list = list
