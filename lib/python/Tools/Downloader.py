0505225bec6d2099f4fa10548ac58439f44bf7e0
ACJTeam <magicobuendia@hotmail.com>
2018-03-28 23:53:09 +0200

    Update Downloader.py

--- lib/python/Tools/Downloader.py
+++ lib/python/Tools/Downloader.py
@@ -4,7 +4,7 @@ from twisted.internet import reactor, defer, ssl
 
 class HTTPProgressDownloader(client.HTTPDownloader):
 	def __init__(self, url, outfile, headers=None):
-		client.HTTPDownloader.__init__(self, url, outfile, headers=headers, agent="Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;teamBlue;;;)")
+		client.HTTPDownloader.__init__(self, url, outfile, headers=headers, agent="Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenACJ;;;)")
 		self.status = self.progress_callback = self.error_callback = self.end_callback = None
 		self.deferred = defer.Deferred()
 
